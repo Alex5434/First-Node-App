92e5c449e31e32914695bdd68f9c2732279bf186
Alex <ezhil2744@gmail.com>
2022-12-26 17:57:55 +0530

    hello world

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.get('/', (req, res)=>{
 
 const start = async()=>{
   try {
-    await connectD(process.env.MOONGO_URI)
+    await connectD('mongodb://alex5434:3Ccw3VB4VUGAiyZY@ac-0sdman7-shard-00-00.i3yfkuq.mongodb.net:27017,ac-0sdman7-shard-00-01.i3yfkuq.mongodb.net:27017,ac-0sdman7-shard-00-02.i3yfkuq.mongodb.net:27017/Task-managerDb?ssl=true&replicaSet=atlas-b96v7q-shard-0&authSource=admin&retryWrites=true&w=majority')
     app.listen(port,console.log(`server running on port ${port}..`))
   } catch (error) {
     console.log(error);
-  }
+  } 
 } 
 
 start()
